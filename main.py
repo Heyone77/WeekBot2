16913721e8a9eb2edad6c6a6aeb1e10728f398a2
d1sq <a.dolgikh@hostco.ru>
2023-03-10 21:19:52 +0500

    feature/2 commit message

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ from dotenv import load_dotenv, find_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
 
+
 class ChatState:
     def __init__(self):
-        self.askers = []
+        self.askers = {}
         self.messages_to_del = []
-        self.schedule_just_asked = False
 
     def msg_to_del(self):
-        print(self.messages_to_del)
+        print(datetime.datetime.now().time(), self.messages_to_del)
+
 
 # Загрузка переменных среды
 load_dotenv(find_dotenv())
@@ -27,14 +28,22 @@ bot = telebot.TeleBot(token)
 scheduler = BackgroundScheduler()
 chat_state = ChatState()
 
-# Словарь для хранения значений
-
 
 # Функция для изменения названия чата
 def change_chat_title():
     title = "(НН) БЕЗ БАБ ББ-40919" if week_oddity() else "(ЧН) БЕЗ БАБ ББ-40919"
     bot.set_chat_title(chat_id, title)
     chat_state.askers.clear()
+
+
+def can_user_use_command(user_id):
+    now = datetime.datetime.now()
+    if user_id in last_command_time:
+        time_since_last_command = now - last_command_time[user_id]
+        if time_since_last_command < datetime.timedelta(minutes=5):
+            return False
+    last_command_time[user_id] = now
+    return True
 
 
 # Отправка ID чата
@@ -48,49 +57,44 @@ def handle_id(message):
 def change_title_command(message):
     change_chat_title()
 
+
 last_command_time = {}
+
 
 @bot.message_handler(commands=['schedule'])
 def mycommand_handler(message):
-    # проверьте, вызывал ли пользователь команду ранее
-    user_id = message.from_user.id
-    now = datetime.datetime.now()
-    if user_id in last_command_time:
-        time_since_last_command = now - last_command_time[user_id]
-        if time_since_last_command < datetime.timedelta(minutes=5):  # разрешите вызов не чаще чем каждые 5 минут
-            return
-    # обновите время последнего вызова команды для пользователя
-    last_command_time[user_id] = now
-
-    # обработка команды
-    try:
+    if can_user_use_command(message.from_user.id):
         chat_state.messages_to_del.append(message.message_id)
-        message_id = bot.send_photo(message.chat.id, day_schedule(), disable_notification=True).id
+        try:
+            message_id = bot.send_photo(
+                message.chat.id, day_schedule(), disable_notification=True).id
+        except:
+            return
         chat_state.messages_to_del.append(message_id)
-    except:
-        chat_state.messages_to_del.append(
-            bot.send_message(message.chat.id, "На удивление пустой день", disable_notification=True).id)
-
-
+        sleep(25)
+        for msg in chat_state.messages_to_del.copy():
+            bot.delete_message(message.chat.id, msg)
+            chat_state.messages_to_del.remove(msg)
+    else:
+        bot.delete_message(message.chat.id, message.message_id)
+        return
 
 
 @bot.message_handler(commands=["week"])
 def handle_text(message):
-        if message.from_user.id not in chat_state.askers:
-            chat_state.askers.append(message.from_user.id)
-            if week_oddity():
-                bot.send_message(message.chat.id, "Нечётная неделя", disable_notification=True)
-            else:
-                bot.send_message(message.chat.id, "Чётная неделя", disable_notification=True)
-        else:
-            bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEHyPdj76H0IMu5B8JC0J3fydn_EXnFeAAC5xYAAkm1YEjpExkRZP9e7i4E", disable_notification=True)
+    if week_oddity():
+        bot.send_message(message.chat.id, "Нечётная неделя",
+                         disable_notification=True)
+    else:
+        bot.send_message(message.chat.id, "Чётная неделя",
+                         disable_notification=True)
+        bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEHyPdj76H0IMu5B8JC0J3fydn_EXnFeAAC5xYAAkm1YEjpExkRZP9e7i4E",
+                         disable_notification=True)
 
 
-
-scheduler.add_job(func=change_chat_title, trigger='cron', day_of_week='mon', hour=15,  timezone='Asia/Yekaterinburg')
-scheduler.add_job(func=chat_state.msg_to_del, trigger='interval', seconds=10)
+scheduler.add_job(func=change_chat_title, trigger='cron',
+                  day_of_week='mon', hour=7, timezone="UTC")
 
 scheduler.start()
 
 bot.polling(none_stop=True, interval=0)
-
