cf0b0e6a4ddb1b9e3a37f58b77b0df352d7e6e36
heyone <heyone3131@gmail.com>
2023-03-14 16:12:09 +0500

    Preparations for GPT

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
+import atexit
+import json
 
 
 class ChatState:
-    def __init__(self):
-        self.askers = {}
+    def __init__(self, askers=None):
+        if askers is None:
+            askers = {}
+        self.askers = askers
         self.messages_to_del = []
 
     def msg_to_del(self):
         print(datetime.datetime.now().time(), self.messages_to_del)
+
+    def get_askers(self):
+        print(self.askers)
+
 
 
 # Загрузка переменных среды
@@ -28,12 +36,15 @@ bot = telebot.TeleBot(token)
 scheduler = BackgroundScheduler()
 chat_state = ChatState()
 
+with open("users.json", "r") as f:
+    chat_state.askers = {int(k): v for k, v in json.load(f).items()}
+
+
 
 # Функция для изменения названия чата
 def change_chat_title():
     title = "(НН) БЕЗ БАБ ББ-40919" if week_oddity() else "(ЧН) БЕЗ БАБ ББ-40919"
     bot.set_chat_title(chat_id, title)
-    chat_state.askers.clear()
 
 
 def can_user_use_command(user_id):
@@ -45,12 +56,19 @@ def can_user_use_command(user_id):
     last_command_time[user_id] = now
     return True
 
+def add_user_to_askers(user_id):
+    if user_id in chat_state.askers:
+        chat_state.askers[user_id] += 1
+    else:
+        chat_state.askers[user_id] = 0
+
+
 
 # Отправка ID чата
 @bot.message_handler(commands=["id"])
 def handle_id(message):
+    add_user_to_askers(message.from_user.id)
     bot.send_message(message.chat.id, message.chat.id)
-
 
 # Ручной вызов функции смены названия чата
 @bot.message_handler(commands=["title"])
@@ -59,6 +77,12 @@ def change_title_command(message):
 
 
 last_command_time = {}
+
+
+def save_data():
+    with open("users.json", "w") as f:
+        # записываем словарь в файл в формате JSON
+        json.dump(chat_state.askers, f)
 
 
 @bot.message_handler(commands=['schedule'])
@@ -93,8 +117,11 @@ def handle_text(message):
 
 
 scheduler.add_job(func=change_chat_title, trigger='cron',
-                  day_of_week='mon', hour=7, timezone="UTC")
+                  day_of_week='tue', hour=14, minute=10, timezone="Asia/Yekaterinburg")
+
+scheduler.add_job(func=chat_state.get_askers, trigger='interval', seconds=10)
 
 scheduler.start()
+atexit.register(save_data)
 
 bot.polling(none_stop=True, interval=0)
