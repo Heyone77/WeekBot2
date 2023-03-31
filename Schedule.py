5d9d41650e51b0366f0ac1d00c8b489bfda41dae
d1sq <a.dolgikh@hostco.ru>
2023-03-31 17:32:30 +0500

    MOTIVATED

--- Schedule.py
+++ Schedule.py
@@ -1,8 +1,12 @@
 from datetime import datetime
+import requests
+from json import loads
+
 
 def get_photo(path: str):
     with open(path, "rb") as img:
         return img.read()
+
 
 def day_schedule():
     weekday = datetime.today().isoweekday()
@@ -11,3 +15,8 @@ def day_schedule():
     else:
         path = f"./schedule_images/{weekday}.png"
         return get_photo(path)
+
+
+def get_motivational_quote():
+    response = requests.get('https://api.goprogram.ai/inspiration')
+    return loads(response.text)
